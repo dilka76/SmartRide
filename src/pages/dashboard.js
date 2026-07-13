71922735647c644c7d4b3cf2d9e70892401908bf
Dilka <dilka1976@gmail.com>
2026-07-13 15:52:58 +0300

    Car photo added

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -20,7 +20,8 @@ export function DashboardPage() {
 
             <hr class="my-4" />
 
-            <h2 class="h5 mb-3">Качване на снимка на автомобил</h2>
+            <h2 class="h5 mb-2">Качване на снимка на автомобил</h2>
+            <p class="text-muted mb-3">Моля качете снимка на Вашия автомобил.</p>
             <div id="dashboardAlert"></div>
             <form id="carPhotoUploadForm" novalidate>
               <div class="mb-3">
